1c4d513e40d618e973fca9792b14cf392710a04a
Md. Tasdikul Islam Khan <ofikhan@outlook.com>
2021-03-21 13:21:23 +0600

    second commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,76 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "sql",
+				weight: 12.3
+			}, {
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "php",
+				weight: 10
+			}, {
+				text: "bootstrap",
+				weight: 9
+			}, {
+				text: "ofi",
+				weight: 15
+			}, {
+				text: "html5",
+				weight: 7
+			}
+		];
+		return words;
+	}
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Support Engineer.", "love everything about code.", "am learning everyday."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#3F0E40"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+}();
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
